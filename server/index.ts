3eaa8a551247264e82484954a0479f892b98599d
Axel Schoenberger <axel.schoenberger@gmail.com>
2024-09-18 17:44:11 +0200

    full path of build/server/index.js

--- server/index.ts
+++ server/index.ts
@@ -202,7 +202,7 @@ async function getBuild() {
 	try {
 		const build = viteDevServer
 			? await viteDevServer.ssrLoadModule('virtual:remix/server-build')
-			: await import('../build/server')
+			: await import('../build/server/index.js')
 
 		return { build: build as unknown as ServerBuild, error: null }
 	} catch (error) {
