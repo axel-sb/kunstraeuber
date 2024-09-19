1590fe57060b6384057273c0d445dff6d86cb6b2
Axel Schoenberger <axel.schoenberger@gmail.com>
2024-09-19 14:41:09 +0200

    now is the time

--- server/index.ts
+++ server/index.ts
@@ -198,19 +198,21 @@ app.use((req, res, next) => {
 	return generalRateLimit(req, res, next)
 })
 
-async function getBuild(): Promise<{ error: unknown; build: ServerBuild | null }> {
-  try {
-    const build = viteDevServer
+async function getBuild(): Promise<{
+	error: unknown
+	build: ServerBuild | null
+}> {
+	try {
+		const build = viteDevServer
 			? await viteDevServer.ssrLoadModule('virtual:remix/server-build')
-			: // @ts-ignore
-				await import('../build/server/index.js')
+			: await import('../build/server/index.js')
 
-    return { build: build as unknown as ServerBuild, error: null };
-  } catch (error) {
-    // Catch error and return null to make express happy and avoid an unrecoverable crash
-    console.error('Error creating build:', error);
-	return { error: error, build: null };
-  }
+		return { build: build as unknown as ServerBuild, error: null }
+	} catch (error) {
+		// Catch error and return null to make express happy and avoid an unrecoverable crash
+		console.error('Error creating build:', error)
+		return { error: error, build: null }
+	}
 }
 
 if (!ALLOW_INDEXING) {
